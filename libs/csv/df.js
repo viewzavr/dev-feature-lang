b754e3a882deef273129d919ac455bda70be99d5
Pavel Vasev <pavel.vasev@gmail.com>
2021-11-30 12:59:33 +0500

    libs

---
+++ libs/csv/df.js
@@ -0,0 +1,123 @@
+// dataframe entity
+
+///////////////////////////////////////////
+// constructors and getters
+///////////////////////////////////////////
+export function create() {
+  var df =  { "colnames": [], length: 0, isDataFrame: true, string_columns: {} }
+  df.clone = clone.bind(df,df);
+  df.is_df = true;
+  return df;
+}
+
+export function add_column( df, name, values ) {
+  if (df.colnames.indexOf(name) < 0)
+    df.colnames.push( name );
+  values ||= [];
+  df[name] = values;
+  if (values.length > df.length) df.length = values.length;
+  return df;
+}
+
+export function get_column_names( df ) {
+  return df.colnames;
+}
+
+export function get_column( df, name ) {
+  return df[name];
+}
+
+export function is_df( df ) {
+  if (df && df.isDataFrame) return true;
+  return false;
+}
+
+export function set_length( df, len ) {
+  df.length = len;
+}
+
+export function get_length( df ) {
+  return df.length;
+}
+
+export function set_column_type_string( df, name ) {
+  df.string_columns[ name ] = true
+}
+
+export function is_string_column( df, name ) {
+  return df.string_columns[ name ] ? true : false
+}
+
+///////////////////////////////////////////
+// algos
+///////////////////////////////////////////
+
+export function create_from_hash( hash ) {
+  var r = create();
+  import_hash( r, hash );
+  return r;
+}
+
+export function import_hash( r, hash ) {
+  Object.keys(hash).forEach( function(name) {
+      add_column( r, name, hash[name] );
+  });
+}
+
+export function update_length( df ) {
+  var cn = get_column_names( df )[0];
+  var col = get_column( df, cn );
+  if (col) set_length( df, col.length );
+  return df;
+}
+
+export function create_from_df( src ) {
+  var r = create();
+
+  get_column_names(src).forEach( function(name) {
+      add_column( r, name, get_column(src,name).slice() );
+  });
+
+  return r;
+}
+
+export function clone( src ) {
+  return create_from_df( src );
+}
+
+export function create_from_df_no_slice( src ) {
+  var r = create();
+
+  get_column_names(src).forEach( function(name) {
+      add_column( r, name, get_column(src,name).slice() );
+  });
+
+  return r;
+}
+
+// оставляет только каждую step-тую строку
+export function skip_every( src,step ) {
+  var r = create();
+  
+
+  get_column_names(src).forEach( function(name) {
+    var newarr = [];
+    var origarr = get_column(src,name);
+    if (step > 0) // мало ли какой step прислалил, зависать мы не должны
+      for (var i=0; i<get_length(src);i += step) newarr.push( origarr[i] );
+    add_column( r, name, newarr );
+  });
+
+  return r;
+}
+
+// выполняет slice на всех колонках
+export function slice( src, index0, index1 ) {
+  var r = create();
+
+  get_column_names(src).forEach( function(name) {
+      add_column( r, name, get_column(src,name).slice(index0,index1) );
+  });
+
+  return r;
+}
---
+++ libs/lib3d/lib3d.js
@@ -0,0 +1,127 @@
+export function setup(vz, m) {
+  vz.register_feature_set( m );
+}
+
+import * as utils from "../utils.js";
+
+export function points( env ) {
+  var obj = env.vz.vis.addPoints( env, "points" );
+  add_std( obj );
+  env.trackParam("input",(df) => {
+    console.log("gonna paint df=",df);
+    var dat = df;
+    obj.positions = utils.combine( [ dat.X, dat.Y, dat.Z ] );
+    obj.colors = utils.combine( [ dat.R, dat.G, dat.B ] );
+    obj.radiuses = dat.RADIUS || [];
+    obj.setParam("count",obj.positions.length / 3);
+    env.signal("changed");
+  })
+}
+
+export function lines( env ) {
+  var obj = env.vz.vis.addLines( env, "lines" );
+  add_std( obj );
+  env.trackParam("input",(df) => {
+    console.log("gonna paint df=",df);
+    var dat = df;
+    obj.positions = utils.combine( [ dat.X, dat.Y, dat.Z, dat.X2, dat.Y2, dat.Z2 ] );
+    if (dat.R2)
+      obj.colors = utils.combine( [ dat.R, dat.G, dat.B,dat.R2, dat.G2, dat.B2 ] );
+    else
+      obj.colors = utils.combine( [ dat.R, dat.G, dat.B, dat.R, dat.G, dat.B ] ); 
+    obj.radiuses = dat.RADIUS || [];
+    obj.setParam("count",obj.positions.length / 3);
+    env.signal("changed");
+  })
+}
+
+// вот такая жизнь.. но есть надежда что на фиче-ланге это будет проще
+export function linestrips( env ) {
+  var convertor_env = env.create_obj({},{name:"convertor"});
+  convertor_env.feature( "linestrips_to_lines" );
+  convertor_env.linkParam( "input","..->input");
+  var painter_env = env.create_obj({},{name:"lines-env"});
+  painter_env.linkParam( "input","@convertor->output");
+  painter_env.feature("lines");
+}
+export function linestrip( env ) {
+  return linestrips(env);
+}
+
+import * as df from "../csv/df.js";
+
+// идея - сведем к lines
+// сделаем просто конвертор
+export function linestrips_to_lines( env ) {
+  env.trackParam("input",(dat) => {
+    var prevn;
+    var vals = {X:[],Y:[],Z:[],X2:[],Y2:[],Z2:[],R:[],G:[],B:[],R2:[],G2:[],B2:[]}
+    var Nvals = dat.N ? dat.N : [];
+    for (var i = 0; i<dat.length; i++)
+    {
+      var n = Nvals[i];
+      if (n == prevn) {
+        if (i > 0) { // todo optimize
+          vals.X.push( dat.X[i-1] );
+          vals.Y.push( dat.Y[i-1] );
+          vals.Z.push( dat.Z[i-1] );
+          vals.X2.push( dat.X[i] );
+          vals.Y2.push( dat.Y[i] );
+          vals.Z2.push( dat.Z[i] );
+
+          if (dat.R) {
+            vals.R.push( dat.R[i-1] );
+            vals.G.push( dat.G[i-1] );
+            vals.B.push( dat.B[i-1] );
+            vals.R2.push( dat.R[i] );
+            vals.G2.push( dat.G[i] );
+            vals.B2.push( dat.B[i] );
+          }
+        }
+      }
+      prevn = n;
+    }
+    if (!dat.R) vals.R=undefined;
+    var newdf = df.create_from_hash( vals );
+    env.setParam("output", newdf)
+  });
+  if (env.params.input) env.signalParam("input");
+}
+
+function add_std( obj ) {
+  obj.addArray("positions",[],3,function(v) {
+    obj.positions = v;
+  } );
+  obj.setParamOption("positions","internal",true);
+  
+  obj.addArray("radiuses",[],1,function(v) {
+    obj.radiuses = v;
+  } );
+  obj.setParamOption("radiuses","internal",true);  
+  
+  obj.addArray("colors",[],1,function(v) {
+    obj.colors = v;
+  } );
+  obj.setParamOption("colors","internal",true);    
+  
+  obj.addCheckbox("visible",true,(v) => {
+    obj.visible=v;
+  });
+
+  obj.addString("count","0");
+
+}
+
+/*
+export function add_css_style( env ) {
+
+  env.trackParam("text",(styles) => {
+    var styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.textContent = styles;
+    document.head.appendChild(styleSheet);
+  })
+  if (env.params.text) env.signalParam("text");
+
+}
+*/
